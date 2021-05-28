13011c8b5a666bda85a8b1e32032eff7e4c5faa0
Conner Leigh <cleigh@constellation.coop>
2021-05-28 15:25:43 -0400

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3080;
+
+// Sets client directory as static so the index file can reference other files
+app.use(express.static(path.join(__dirname, "/client/public")));
+
+// JSON parser for incoming request bodies
+app.use(express.json());
+
+// Serves html file when root directory is hit
+app.get("/", (req, res) => res.sendFile("/client/build/index.html"));
+
+// Listener for post request with this specific route
+app.post("/test", (req, res) => {
+        res.json({ success: true, message: "That's correct!" });
+})
+
+// Listens for provided port and runs server
+app.listen(PORT, () => console.log(`🌎 ==> Server now on port ${PORT}!`))
