6f9281f591eb80d9298ab36cb3d4ccabb37c1db4
Conner Leigh <cjay741@gmail.com>
2024-02-16 19:33:53 -0500

    making port env variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3080;
+const PORT = process.env.PORT;
 
 // Define middleware here
 app.use(express.json({ limit: "50mb" }));
